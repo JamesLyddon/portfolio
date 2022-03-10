b7dcab085954239ef632ca890e70bf325a9024ca
JamesLyddon <jameslyddon@gmail.com>
2022-03-10 13:26:40 +0000

    added some basic 3d animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,31 @@
 import Acomplishments from "../components/Acomplishments/Acomplishments";
 import BgAnimation from "../components/BackgrooundAnimation/BackgroundAnimation";
+import Cube from "../components/Cube/Cube";
+import Floor from "../components/Floor/Floor";
 import Hero from "../components/Hero/Hero";
 import Projects from "../components/Projects/Projects";
 import Technologies from "../components/Technologies/Technologies";
 import Timeline from "../components/TimeLine/TimeLine";
 import { Layout } from "../layout/Layout";
 import { Section } from "../styles/GlobalComponents";
+import { Canvas } from "@react-three/fiber";
+import { RGBA_ASTC_10x10_Format } from "three";
 
 const Home = () => {
   return (
     <Layout>
       <Section grid>
         <Hero />
-        <BgAnimation />
+        <Canvas
+          shadows={true}
+          camera={{
+            position: [-6, 7, 7],
+          }}
+        >
+          <ambientLight color="white" intensity={1} />
+          <Cube />
+          <Floor position={[0, -5, 0]} />
+        </Canvas>
       </Section>
       <Projects />
       <Technologies />
